0ae956ec20f2190d516ed3af317cfe70508c73e1
Harry Reger <hreger@hareg.com>
2025-09-18 11:12:50 +0200

    4523 - 4524

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,34 @@ class S4523 extends StatelessWidget {
   const S4523({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  const MyNameWidget({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyNameWidgetState();
+}
+
+class _MyNameWidgetState extends State<MyNameWidget> {
+  
+  String name = '';
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: 16,
+      children: [
+        Text(name),
+        ElevatedButton(onPressed: () {
+          setState(() {
+            name = 'Harry Reger';
+          });
+        }, child: Text('Name anzeigen'))
+      ],
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,40 @@ class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ShowHideNameWidget();
+  }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+    
+  @override
+  State<StatefulWidget> createState() => _ShowHideNameWidgetState();
+
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  bool _isNameVisible = false;
+  String name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: 16,
+      children: [
+        Text(name),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _isNameVisible = !_isNameVisible;
+              name = _isNameVisible ? 'Harry Reger' : '';
+            });
+          },
+          child: Text(_isNameVisible ? 'Name verstecken' : 'Name anzeigen'),
+        ),
+      ],
+    );
   }
 }
